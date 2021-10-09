5ed4c08a466eec3305b01b11abc0c93235f393df
David E. DeMarle <david.demarle@intel.com>
2021-10-08 21:28:56 -0400

    finish the small simple example
    seems to be more or less correct. An observation is that
    remote value treatment is tricky, NAN is the worst, the original
    approach of setting remotes to 0 and letting the LUT handle it
    is pretty good but boundaries are fairly visible where they
    transition to the 0 color. Setting remote values to local value is
    quite close, but there are still visible boundaries. Setting it to
    the actual value is closer still but even that has some artifacts.

--- modules/mpi/tutorials/ospMPIDistribTutorialAMR.cpp
+++ modules/mpi/tutorials/ospMPIDistribTutorialAMR.cpp
@@ -18,6 +18,9 @@
 #include "ospray/ospray_cpp/ext/rkcommon.h"
 #include "ospray/ospray_util.h"
 #include "GravitySpheresVolume.h"
+#include "wombat.h"
+
+int rankToDebugf = -1;
 
 using namespace ospray;
 using namespace rkcommon;
@@ -41,6 +44,7 @@ static box3f worldBounds;
 // Generate the rank's local volume brick
 VolumeBrick makeGravitySpheresVolume(const int mpiRank, const int mpiWorldSize);
 VolumeBrick makeSimpleVolume1(const int mpiRank, const int mpiWorldSize);
+VolumeBrick makeFakeMPIVolume(const int fakeWorldSize);
 
 bool trustInWombat = true;
 
@@ -64,6 +68,7 @@ int main(int argc, char **argv)
   std::cout << "OSPRay rank " << mpiRank << "/" << mpiWorldSize << "\n";
 
   int scene = 0;
+  int fakeWorldSize = 1;
   for (int i = 0; i < argc; ++i)
   {
     if (!strcmp(argv[i], "-noWombatNoCry")) {
@@ -76,7 +81,18 @@ int main(int argc, char **argv)
     if (!strcmp(argv[i], "-Simple1")) {
       scene = 1;
     }
+    if (!strcmp(argv[i], "-Fake")) {
+      scene = 2;
+      if (i+1 < argc)
+      {
+        fakeWorldSize = std::stoi(std::string(argv[i+1]));
+      }
+    }
+
   }
+
+  const char *rankToDebugfEnv = getenv("RANKTODEBUGF");
+  if (rankToDebugfEnv) rankToDebugf = std::stoi(std::string(rankToDebugfEnv));
 
   // load the MPI module, and select the MPI distributed device. Here we
   // do not call ospInit, as we want to explicitly pick the distributed
@@ -109,6 +125,10 @@ int main(int argc, char **argv)
         break;
     case 1:
         brick = makeSimpleVolume1(mpiRank, mpiWorldSize);
+        break;
+    case 2:
+        brick = makeFakeMPIVolume(fakeWorldSize);
+        break;
     }
 
     // create the "world" model which will contain all of our geometries
@@ -164,7 +184,7 @@ int main(int argc, char **argv)
 
 VolumeBrick makeGravitySpheresVolume(const int mpiRank, const int mpiWorldSize)
 {
-  if (!mpiRank) std::cerr << "MAKING GRAVITY SPHERES VOLUME #1" << std::endl;
+  if (mpiRank==rankToDebugf) std::cerr << "MAKING GRAVITY SPHERES VOLUME #1" << std::endl;
   VolumeBrick brick;
 
   GravitySpheres gs(true, mpiRank, mpiWorldSize);
@@ -209,62 +229,166 @@ VolumeBrick makeGravitySpheresVolume(const int mpiRank, const int mpiWorldSize)
 
 VolumeBrick makeSimpleVolume1(const int mpiRank, const int mpiWorldSize)
 {
-  if (!mpiRank) std::cerr << "MAKING SIMPLE VOLUME #1" << std::endl;
+  if (mpiRank==rankToDebugf) std::cerr << "MAKING SIMPLE VOLUME #1" << std::endl;
 
   cpp::Volume volume("amr");
 
   std::vector<std::vector<float>> perBlockData;
-  std::vector<box3i> perBlockBounds;
-  std::vector<box3f> localPerBlockBounds;
+  std::vector<box3i> perBlockExtents;
+  std::vector<box3f> perBlockBounds;
   std::vector<int> perBlockLevels;
   std::vector<int> perBlockOwners;
   std::vector<float> perLevelCellWidths;
 
-  int blockDims = 16;
-  int numlevels = 1;
-  for (int level = 0; level < numlevels; level++) {
-    for (int r = 0; r < mpiWorldSize; r++) {
-      std::cerr << r << std::endl;
-      float v = r;
+  worldBounds = box3f(vec3f(0.f), vec3f(0.f));
+
+  int baseRes = 2;
+  const char *baseResEnv = getenv("BASERES");
+  if (baseResEnv) baseRes = std::stoi(std::string(baseResEnv));
+
+  int numLevels = 2;
+  const char *numLevelsEnv = getenv("NUMLEVELS");
+  if (numLevelsEnv) numLevels = std::stoi(std::string(numLevelsEnv));
+
+  int remoteValueChoice = 0;
+  const char *remoteValueChoiceEnv = getenv("REMOTEVALUECHOICE");
+  if (remoteValueChoiceEnv) remoteValueChoice = std::stoi(std::string(remoteValueChoiceEnv));
+
+  int blockDims = baseRes*mpiWorldSize;
+  for (int level = 0; level < numLevels; level++) {
+    int numInnerBlocks = powf(mpiWorldSize, level+1);
+    float w = 1.f / powf(mpiWorldSize, level);
+    if (mpiRank==rankToDebugf) std::cerr << "L " << level << " w " << w << std::endl;
+    perLevelCellWidths.push_back(w);
+
+    for (int r = 0; r < numInnerBlocks; r++) {
+      if (mpiRank==rankToDebugf) std::cerr << "blocknum " << r << std::endl;
+      int owner = r%mpiWorldSize;
+
+      if (mpiRank==rankToDebugf) std::cerr << "owner " << owner << std::endl;
+      float v = owner+1;
+      if (owner != mpiRank) {
+        if (remoteValueChoice == 1) v = 0.0;
+        if (remoteValueChoice == 2) v = NAN;
+        if (remoteValueChoice == 3) v = mpiRank+1;
+      }
+      if (mpiRank==rankToDebugf) std::cerr << "value " << v << std::endl;
       std::vector<float> data(blockDims*blockDims*blockDims, v);
-      perBlockData.push_back(data);
-      std::cerr << "data " << data.size() << std::endl;
+      if (mpiRank==rankToDebugf) std::cerr << "data " << data.size() << " " << v << "'s" << std::endl;
 
       box3i box;
       box.lower = vec3i(r*blockDims,0,0);
       box.upper = vec3i((r+1)*blockDims-1,blockDims-1,blockDims-1);
-      std::cerr << "box " << box << std::endl;
-      perBlockBounds.push_back(box);
+      if (mpiRank==rankToDebugf) std::cerr << "box " << box << std::endl;
+
+      box3f boxf;
+      boxf.lower = box.lower*w;
+      boxf.upper = (box.upper+vec3i(1))*w;
+      if (mpiRank==rankToDebugf) std::cerr << "boxf " << boxf << std::endl;
+      worldBounds.extend(boxf);
+
+      //if (owner != mpiRank)
+      //  continue;
 
       perBlockLevels.push_back(level);
-
-      perBlockOwners.push_back(r);
+      perBlockOwners.push_back(owner);
+      perBlockData.push_back(data);
+      perBlockExtents.push_back(box);
+      perBlockBounds.push_back(boxf);
     }
-    float w = 1.f / powf(2, level);
-    std::cerr << "L " << level << " w " << w << std::endl;
-    perLevelCellWidths.push_back(w);
   }
 
   std::vector<cpp::CopiedData> allBlockData;
   for (const std::vector<float> &bd : perBlockData)
     allBlockData.emplace_back(bd.data(), OSP_FLOAT, bd.size());
 
-  worldBounds = box3f(vec3f(0.f), vec3f(mpiWorldSize*blockDims, blockDims, blockDims));
   volume.setParam("gridOrigin", vec3f(0.f));
   volume.setParam("gridSpacing", vec3f(1.f));
   volume.setParam("block.data", cpp::CopiedData(allBlockData));
-  volume.setParam("block.bounds", cpp::CopiedData(perBlockBounds));
+  volume.setParam("block.bounds", cpp::CopiedData(perBlockExtents));
   volume.setParam("block.level", cpp::CopiedData(perBlockLevels));
   volume.setParam("cellWidth", cpp::CopiedData(perLevelCellWidths));
   volume.commit();
 
+  if (trustInWombat) {
+    std::vector<box3f> wRegions;
+
+    std::vector<wombat::Level> wLevels;
+    std::vector<vec3i> wLevelDims;
+    vec3i l0dims(blockDims*mpiWorldSize,blockDims,blockDims); //top level dims, globally
+    int prevRefRatio = 1;
+    for (auto n : perLevelCellWidths)
+    {
+      wombat::Level l;
+      l.refinement[0] = mpiWorldSize;
+      l.refinement[1] = mpiWorldSize;
+      l.refinement[2] = mpiWorldSize;
+      wLevels.push_back(l);
+      wLevelDims.push_back(l0dims * prevRefRatio);
+      prevRefRatio = prevRefRatio * mpiWorldSize;
+    }
+    std::vector<wombat::Box> wBoxes;
+    const std::vector<int> &wBlockOwners = perBlockOwners;
+    const std::vector<int> &wBlockLevels = perBlockLevels;
+    int index = 0;
+    for (auto n : perBlockExtents)
+    {
+      wombat::Box b;
+      b.owningrank = wBlockOwners[index];
+      b.level = wBlockLevels[index];
+      b.origin[0] = n.lower.x;
+      b.origin[1] = n.lower.y;
+      b.origin[2] = n.lower.z;
+      vec3f bo(b.origin[0],b.origin[1],b.origin[2]);
+      b.dims[0] = n.upper.x-n.lower.x+1;
+      b.dims[1] = n.upper.y-n.lower.y+1;
+      b.dims[2] = n.upper.z-n.lower.z+1;
+      vec3f bd(b.dims[0],b.dims[1],b.dims[2]);
+      vec3f bx0 = (vec3f(bo)/(vec3f(wLevelDims[b.level]))) * worldBounds.upper;
+      vec3f bx1 = (vec3f(bo+bd)/(vec3f(wLevelDims[b.level]))) * worldBounds.upper;
+      if (mpiRank==rankToDebugf) std::cerr << "IN:  " << index << " " << b.owningrank << " " << b.level << " "
+                << bo << "+" << bd << "|"
+                << bx0 << ".." << bx1 << std::endl;
+      wBoxes.push_back(b);
+      index++;
+    }
+
+    std::vector<wombat::Box> wsBoxes;
+    //run wombat to derive a set of convex regions
+    wombat::geneology(wLevels, wBoxes, mpiRank);
+    wombat::convexify(wLevels, wBoxes, wsBoxes);
+
+    //from that run, tell compositor the regions that this rank owns in worldspace
+    index = 0;
+    for (; index < wsBoxes.size(); ++index)
+    {
+      wombat::Box b = wsBoxes[index];
+      vec3i bo = b.origin;
+      vec3i bd = b.dims;
+      vec3f bx0 = ((vec3f(bo)+vec3f(0.f))/(vec3f(wLevelDims[b.level]))) * worldBounds.upper;
+      vec3f bx1 = ((vec3f(bo+bd)-vec3f(0.f))/(vec3f(wLevelDims[b.level]))) * worldBounds.upper;
+      if (mpiRank==rankToDebugf) std::cerr << "OUT: " << index << " " << (b.owningrank==mpiRank?"*":"") << b.owningrank << " " << b.level << " "
+                                           << bo << "+" << bd << "|"
+                                           << bx0 << ".." << bx1 << std::endl;
+      if (b.owningrank == mpiRank)
+      {
+        wRegions.push_back(box3f(vec3f(bx0.x,bx0.y,bx0.z),vec3f(bx1.x,bx1.y,bx1.z)));
+      }
+    }
+    perBlockBounds = wRegions;
+  }
+
   cpp::VolumetricModel model = cpp::VolumetricModel(volume);
   cpp::TransferFunction tfn("piecewiseLinear");
   std::vector<vec3f> colors = {vec3f(0.f, 0.f, 1.f), vec3f(1.f, 0.f, 0.f)};
-  std::vector<float> opacities = {0.25f, 1.0f};
+  std::vector<float> opacities;
+  opacities.push_back(0.0);
+  for (int i = 0; i < mpiWorldSize; i++)
+    opacities.push_back(0.95);
+
   tfn.setParam("color", cpp::CopiedData(colors));
   tfn.setParam("opacity", cpp::CopiedData(opacities));
-  vec2f valueRange = vec2f(0, mpiWorldSize);
+  vec2f valueRange = vec2f(0, mpiWorldSize+1);
   tfn.setParam("valueRange", valueRange);
   tfn.commit();
   model.setParam("transferFunction", tfn);
@@ -283,9 +407,118 @@ VolumeBrick makeSimpleVolume1(const int mpiRank, const int mpiWorldSize)
   brick.model = model;
   brick.group = group;
   brick.instance = instance;
-  localPerBlockBounds.push_back(worldBounds); //todo - correct for wombat
-  brick.bounds = localPerBlockBounds;
-  brick.ghostBounds = worldBounds;
+  brick.bounds = perBlockBounds;
+  brick.ghostBounds = worldBounds; //unused so far
+
+  return brick;
+}
+
+VolumeBrick makeFakeMPIVolume(const int mpiWorldSize)
+{
+  if (rankToDebugf>-1) std::cerr << "MAKING SIMPLE VOLUME #1" << std::endl;
+
+  cpp::Volume volume("amr");
+
+  std::vector<std::vector<float>> perBlockData;
+  std::vector<box3i> perBlockExtents;
+  std::vector<box3f> perBlockBounds;
+  std::vector<int> perBlockLevels;
+  std::vector<int> perBlockOwners;
+  std::vector<float> perLevelCellWidths;
+
+  worldBounds = box3f(vec3f(0.f), vec3f(0.f));
+
+  int baseRes = 2;
+  const char *baseResEnv = getenv("BASERES");
+  if (baseResEnv) baseRes = std::stoi(std::string(baseResEnv));
+
+  int numLevels = 2;
+  const char *numLevelsEnv = getenv("NUMLEVELS");
+  if (numLevelsEnv) numLevels = std::stoi(std::string(numLevelsEnv));
+
+  int remoteValueChoice = 0;
+  const char *remoteValueChoiceEnv = getenv("REMOTEVALUECHOICE");
+  if (remoteValueChoiceEnv) remoteValueChoice = std::stoi(std::string(remoteValueChoiceEnv));
+
+  int blockDims = baseRes*mpiWorldSize;
+  for (int level = 0; level < numLevels; level++) {
+    int numInnerBlocks = powf(mpiWorldSize, level+1);
+    float w = 1.f / powf(mpiWorldSize, level);
+    if (rankToDebugf>-1) std::cerr << "L " << level << " w " << w << std::endl;
+    perLevelCellWidths.push_back(w);
+
+    for (int r = 0; r < numInnerBlocks; r++) {
+      if (rankToDebugf>-1) std::cerr << "blocknum " << r << std::endl;
+      int owner = r%mpiWorldSize;
+
+      if (rankToDebugf>-1) std::cerr << "owner " << owner << std::endl;
+      float v = owner+1;
+      if (rankToDebugf>-1) std::cerr << "value " << v << std::endl;
+      std::vector<float> data(blockDims*blockDims*blockDims, v);
+      if (rankToDebugf>-1) std::cerr << "data " << data.size() << " " << v << "'s" << std::endl;
+
+      box3i box;
+      box.lower = vec3i(r*blockDims,0,0);
+      box.upper = vec3i((r+1)*blockDims-1,blockDims-1,blockDims-1);
+      if (rankToDebugf>-1) std::cerr << "box " << box << std::endl;
+
+      box3f boxf;
+      boxf.lower = box.lower*w;
+      boxf.upper = (box.upper+vec3i(1))*w;
+      if (rankToDebugf>-1) std::cerr << "boxf " << boxf << std::endl;
+      worldBounds.extend(boxf);
+
+      perBlockLevels.push_back(level);
+      perBlockOwners.push_back(owner);
+      perBlockData.push_back(data);
+      perBlockExtents.push_back(box);
+      perBlockBounds.push_back(boxf);
+    }
+  }
+
+  std::vector<cpp::CopiedData> allBlockData;
+  for (const std::vector<float> &bd : perBlockData)
+    allBlockData.emplace_back(bd.data(), OSP_FLOAT, bd.size());
+
+  volume.setParam("gridOrigin", vec3f(0.f));
+  volume.setParam("gridSpacing", vec3f(1.f));
+  volume.setParam("block.data", cpp::CopiedData(allBlockData));
+  volume.setParam("block.bounds", cpp::CopiedData(perBlockExtents));
+  volume.setParam("block.level", cpp::CopiedData(perBlockLevels));
+  volume.setParam("cellWidth", cpp::CopiedData(perLevelCellWidths));
+  volume.commit();
+
+  cpp::VolumetricModel model = cpp::VolumetricModel(volume);
+  cpp::TransferFunction tfn("piecewiseLinear");
+  std::vector<vec3f> colors = {vec3f(0.f, 0.f, 1.f), vec3f(1.f, 0.f, 0.f)};
+  std::vector<float> opacities;
+  opacities.push_back(0.0);
+  for (int i = 0; i < mpiWorldSize; i++)
+    opacities.push_back(0.95);
+
+  tfn.setParam("color", cpp::CopiedData(colors));
+  tfn.setParam("opacity", cpp::CopiedData(opacities));
+  vec2f valueRange = vec2f(0, mpiWorldSize+1);
+  tfn.setParam("valueRange", valueRange);
+  tfn.commit();
+  model.setParam("transferFunction", tfn);
+  model.setParam("samplingRate", 0.01f);
+  model.commit();
+
+  cpp::Group group = cpp::Group();
+  group.setParam("volume", cpp::CopiedData(model));
+  group.commit();
+
+  cpp::Instance instance = cpp::Instance(group);
+  instance.commit();
+
+  VolumeBrick brick;
+  brick.brick = volume;
+  brick.model = model;
+  brick.group = group;
+  brick.instance = instance;
+  brick.bounds = perBlockBounds;
+  brick.ghostBounds = worldBounds; //unused so far
 
   return brick;
 }
